cb7e46a4a57f5de897c59cdde898f0fbf28644f2
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-05-22 22:34:46 +0100

    feat(frontend): dashboard home component

---
+++ src/pages/dashboard/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import DashboardLayout from '../../../layouts/dashboard.layout'
+
+import './dash.scss'
+
+const Dashboard = () => {
+  return (
+    <DashboardLayout>
+      <div className='dash-home'>
+        <h1>Dashboard home</h1>
+      </div>
+    </DashboardLayout>
+  )
+}
+
+export default Dashboard
