8c6a0d97f5d3bb9ae6609a8370d296659e873080
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-05-23 09:53:56 +0100

    feat(frontend): notes

---
+++ src/pages/dashboard/notes/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import DashboardLayout from '../../../layouts/dashboard.layout'
+
+const Notes = () => {
+  return (
+    <DashboardLayout>
+      <h1>Shared Notes</h1>
+    </DashboardLayout>
+  )
+}
+
+export default Notes
