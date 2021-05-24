9e2f7a72451575f02db4cc6cca0be48b2643f47a
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-05-24 12:14:06 +0100

    feat(frontend): dashboard home

--- src/pages/dashboard/home/index.js
+++ src/pages/dashboard/home/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
+
 import DashboardLayout from '../../../layouts/dashboard.layout'
+import Button from '../../../components/button/button'
+import Card from '../../../components/card/card'
 
 import './dash.scss'
 
@@ -8,6 +11,50 @@ const Dashboard = () => {
     <DashboardLayout>
       <div className='dash-home'>
         <h1>Dashboard</h1>
+        <div className='card-container'>
+          <Card title='agenda' icon='/images/home.svg'>
+            <div className='wrapper'>
+              <div className='form-control'>
+                <input type='checkbox' name='checkbox' id='checkbox' />
+                <label htmlFor='checkbox'>Task 1</label>
+              </div>
+
+              <div className='form-control'>
+                <input type='checkbox' name='checkbox' id='checkbox' />
+                <label htmlFor='checkbox'>Task 2</label>
+              </div>
+
+              <div className='form-control'>
+                <input type='checkbox' name='checkbox' id='checkbox' />
+                <label htmlFor='checkbox'>Task 3</label>
+              </div>
+            </div>
+            <Button icon='/images/plus.svg' text='Add Checkbox' />
+          </Card>
+          <Card title='Personal Notes' icon='/images/file.svg'>
+            <div className='wrapper'>
+              <div className='content'>
+                <p>Instructions:</p>
+                <p>
+                  This is an input text field you need to create, make sure that
+                  the area for text is with correct padding. Limit text to 500
+                  characters.
+                </p>
+                <p>This text must be stored in a Firebase realtime database.</p>
+              </div>
+              <p className='warning'>Max 500 characters</p>
+            </div>
+            <Button icon='/images/plus.svg' text='Check Hover State' />
+          </Card>
+          <Card title='Your Location' icon='/images/file.svg'>
+            <div className='wrapper'>
+              <div className='content'>
+                <p>Country: Sweden</p>
+                <p>Continent: Europe</p>
+              </div>
+            </div>
+          </Card>
+        </div>
       </div>
     </DashboardLayout>
   )
