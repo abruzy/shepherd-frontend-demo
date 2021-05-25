a34de2b6d2415ca50497861bd18ad9bf875719d6
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-05-25 13:27:12 +0100

    feat(frontend): firebase config setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './containers/app'
 import reportWebVitals from './reportWebVitals'
+import firebase from 'firebase/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
+})
 
 ReactDOM.render(
   <React.StrictMode>
